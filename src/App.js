ef87318051e51f61d1afbcade1ee47c4347c7525
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-10 17:54:57 +0530

    clientDump

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DisplayText from './component/DisplayText';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayText/>
+    </div>
+  );
+}
+
+export default App;
